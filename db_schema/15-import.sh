53b8e94f4a504c8978b0f2c44901d02b78b657cf
root <root@ip-172-31-19-134.eu-west-2.compute.internal>
2018-12-08 09:28:05 +0000

    psql logical replication

---
+++ db_schema/15-import.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cp -f /docker-entrypoint-initdb.d/20-schema.xxx /tmp/20-schema.yyy
+cp -f /docker-entrypoint-initdb.d/40-schema.xxx /tmp/40-schema.yyy
+
+
+
+sed -i "s/PGDB_USER/$PGSQL_USER/g" /tmp/20-schema.yyy
+sed -i "s/PGDB_PASSWORD/$PGSQL_PASS/g" /tmp/20-schema.yyy
+sed -i "s/PGDB_NAME/$PGSQL_DBNAME/g" /tmp/20-schema.yyy
+
+sed -i "s/PDNS_PGDB_REPLICA_HOST/$PDNS_PGDB_REPLICA_HOST/g" /tmp/40-schema.yyy
+sed -i "s/PDNS_PGDB_REPLICA_PASSWORD/$PDNS_PGDB_REPLICA_PASSWORD/g" /tmp/40-schema.yyy
+sed -i "s/PDNS_PGDB_REPLICA_USERNAME/$PDNS_PGDB_REPLICA_USERNAME/g" /tmp/40-schema.yyy
+sed -i "s/PDNS_PGDB_REPLICA_DBNAME/$PDNS_PGDB_REPLICA_DBNAME/g" /tmp/40-schema.yyy
+
+psql  < /tmp/20-schema.yyy
+
+psql -U ${PGSQL_USER} ${PGSQL_DBNAME} < /docker-entrypoint-initdb.d/30-schema.xxx
+
+psql ${PGSQL_DBNAME} < /tmp/40-schema.yyy
+
+
+#unset PGSQL_PASS
+#export PGSQL_PASS=xxxxxxxxxxxxxxxxxxx
+
+#rm /tmp/20-schema.yyy
+
+
+ 
